fc47b476d5acac38e309a098e2fe88c0439c5830
shuhei-fujimura <s.fujimura0406@gmail.com>
2024-11-03 07:22:54 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,7 +0,0 @@
-const btn =document.getElementById('btn');
-const parentList = document.getElementById('parent-list');
-btn.addEventListener('click', () => {
-    const childList = document.createElement('h2');
-    childList.textContent = 'ボタンをクリックしました';
-    parentList.appendChild(childList);
-});
